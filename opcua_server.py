095350beb864d575fecf83d8e515e54d638b21f3
iXenios <schmidt_fabio@web.de>
2024-05-14 09:27:05 +0200

    update for opcua server and added tcp_socket

--- opcua_server.py
+++ opcua_server.py
@@ -3,47 +3,17 @@ from asyncua.ua import ObjectIds
 import netifaces as ni
 import datetime
 import asyncio
-import datetime
-
-#GPIOs konfigurieren
-#import RPi.GPIO as GPIO
 import time
-import os
-
-# Zählweise der Pins auf GPIO-Nummern festlegen
-#GPIO.setmode(GPIO.BCM);
-
-# Warnmeldungen ausschalten
-#GPIO.setwarnings(False);
-
-# Konstanten definieren
-luefterPIN = 13
-
-# GPIO Eingänge definieren
-#GPIO.setup(luefterPIN, GPIO.OUT)
-#GPIO.output(luefterPIN, GPIO.LOW)
-
-# PWM-Channel initialisieren
-#pwm1 = GPIO.PWM(luefterPIN, 100)
-#pwm1.start(100.0)
-
-#def setFanDuty(sfd = 0.0): # Setzt den Dutycycle (in % von 0.0-100.0) für den Lüfter
-#    pwm1.ChangeDutyCycle(sfd)
-#    return
-        
-#def startFan(): # Setzt den Lüfter für 1s Anlaufzeit auf 100%
-#    fanOutput = 100.0
-#    setFanDuty(fanOutput)
-#    print("Anlaufzeit: 1s")
-#    time.sleep(1)
-#    return
+import adafruit_dht
+import board
 
 async def main():
-    server=Server()
+    server = Server()
     await server.init()
-    #Get the ip address
-    IPV4_Address = ni.ifaddresses('en0')[ni.AF_INET][0]['addr']
-    url="opc.tcp://"+IPV4_Address+":4840"
+    
+    # Get the IP address
+    IPV4_Address = ni.ifaddresses('wlan0')[ni.AF_INET][0]['addr']
+    url = "opc.tcp://" + IPV4_Address + ":4840"
     server.set_endpoint(url)
 
     server.set_security_policy(
@@ -51,43 +21,47 @@ async def main():
             ua.SecurityPolicyType.NoSecurity
         ])
 
-    #OPCUA Namensraum festleben
-    name="OPCUA_Musterplatine"
-    addspace= await server.register_namespace(name)
+    # OPC UA Namespace
+    name = "OPCUA_Musterplatine"
+    addspace = await server.register_namespace(name)
 
     dev = await server.nodes.base_object_type.add_object_type(addspace, "FBS-Platine")
-    await (await dev.add_variable(addspace, "sensor", 1.0)).set_modelling_rule(True)
-    await (await dev.add_variable(addspace, "luefter", 0)).set_modelling_rule(True)
-    await (await dev.add_variable(addspace, "time", datetime.datetime.utcnow())).set_modelling_rule(True)
+    await (await dev.add_variable(addspace, "Temperature", 0.0)).set_modelling_rule(True)
+    await (await dev.add_variable(addspace, "Humidity", 0.0)).set_modelling_rule(True)
 
-    # First a folder to organise our nodes
     myfolder = await server.nodes.objects.add_folder(addspace, "Raspi")
-
-    # instanciate one instance of our device
     mydevice = await myfolder.add_object(addspace, "FBS-Platine", dev)
 
-    # get proxy to child-elements
-    sensor = await mydevice.get_child(
-        [f"{addspace}:sensor"]
-    )
-    luefter = await mydevice.get_child(
-        [f"{addspace}:luefter"]
-    )
-    time = await mydevice.get_child(
-        [f"{addspace}:time"]
-    )
+    temperature_node = await mydevice.get_child([f"{addspace}:Temperature"])
+    humidity_node = await mydevice.get_child([f"{addspace}:Humidity"])
 
-    await luefter.set_writable()
+    await temperature_node.set_writable()
+    await humidity_node.set_writable()
 
-    #OPCUA-Server starten
+    dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
+
     async with server:
-        print("Server startet auf {}",format(url))
+        print("Server started at", url)
 
         while True:
-            now = datetime.datetime.now()
-            await asyncio.sleep(2)
-            print("Zeit: ", now)
+            try:
+                temperature_c = dhtDevice.temperature
+                humidity = dhtDevice.humidity
 
+                # Update node values
+                await temperature_node.write_value(ua.DataValue(temperature_c))
+                await humidity_node.write_value(ua.DataValue(humidity))
+
+                print("Temperature: {:.2f} C, Humidity: {:.2f}%".format(temperature_c, humidity))
+            except RuntimeError as error:
+                print(error.args[0])
+                time.sleep(2.0)
+                continue
+            except Exception as error:
+                print(error)
+                continue
+
+            await asyncio.sleep(2)
 
 if __name__ == "__main__":
     asyncio.run(main())
