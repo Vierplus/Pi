e035cc9486fc3a4d8a1e77423dbca96989a95ad1
iXenios <schmidt_fabio@web.de>
2024-05-21 09:33:44 +0200

    update OPCUA Server

--- opcua_server/__main__.py
+++ opcua_server/__main__.py
@@ -1,16 +1,25 @@
-import asyncio
+import asyncio 
 from asyncua import Server, ua
+from asyncua.ua import ObjectIds
+import netifaces as ni
+import datetime
+import asyncio
+import time
+import adafruit_dht
+import board
 
 
 async def update_values(temperature_var, humidity_var):
-    new_temperature = await temperature_var.get_value() + 1.0  # Simulate updating the temperature
-    new_humidity = await humidity_var.get_value() + 1.0  # Simulate updating the humidity
+    dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
 
-    await temperature_var.write_value(new_temperature)
-    await humidity_var.write_value(new_humidity)
+    temperature_c = dhtDevice.temperature
+    humidity = dhtDevice.humidity
 
-    print(f"Updated Temperature to {new_temperature} and Humidity to {new_humidity}")
+    # Update node values
+    await temperature_c.write_value(ua.DataValue(temperature_c))
+    await humidity.write_value(ua.DataValue(humidity))
 
+    print("Temperature: {:.2f} C, Humidity: {:.2f}%".format(temperature_c, humidity))
 
 async def opc_ua_server():
     # Setup the server
@@ -25,15 +34,24 @@ async def opc_ua_server():
     server.set_security_policy([ua.SecurityPolicyType.NoSecurity])
 
     # Namespace setup
-    uri = "http://example.com/Messestand"  # Use a generic, stable URI
+    uri = "VierPlus"  # Use a generic, stable URI
     idx = await server.register_namespace(uri)
 
     # Add an object to the address space
-    messestand = await server.nodes.objects.add_object(idx, "Messestand")
-    temperature = await messestand.add_variable(idx, "Temperature", 0.0)
-    humidity = await messestand.add_variable(idx, "Humidity", 0.0)
+    dev = await server.nodes.objects.add_object(idx, "Messestand")
+    await (await dev.add_variable(idx, "Temperature", 0.0)).set_modelling_rule(True)
+    await (await dev.add_variable(idx, "Humidity", 0.0)).set_modelling_rule(True)
+
+    myfolder = await server.nodes.objects.add_folder(idx, "Raspi")
+    mydevice = await myfolder.add_object(idx, "FBS-VierPlus", dev)
+
+    temperature = await mydevice.get_child([f"{idx}:Temperature"])
+    humidity = await mydevice.get_child([f"{idx}:Humidity"])
+
     await temperature.set_writable()
     await humidity.set_writable()
+
+    
 
     print("Starting OPC UA server at " + url)
 
@@ -41,7 +59,15 @@ async def opc_ua_server():
     async with server:
         print("OPC UA server is running and waiting for requests...")
         while True:
-            await update_values(temperature, humidity)  # Update the values
+            try:
+                await update_values(temperature, humidity)  # Update the values
+            except RuntimeError as error:
+                print(error.args[0])
+                time.sleep(2.0)
+                continue
+            except Exception as error:
+                print(error)
+                continue
             await asyncio.sleep(5)  # Adjust the sleep duration as needed
 
 
